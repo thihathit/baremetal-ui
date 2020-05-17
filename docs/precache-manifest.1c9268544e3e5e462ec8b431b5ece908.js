e24d74aaa724a92912dc6917d4d7dd4f86e424b6
Thiha <developer.thiha@gmail.com>
2020-05-18 02:40:25 +0630

    Export Docs for gh-pages

---
+++ docs/precache-manifest.1c9268544e3e5e462ec8b431b5ece908.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cbe1266095e0eab15ae3fb709d12e9b",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.10446b5352d390040fa7.bundle.js"
+  },
+  {
+    "url": "runtime~main.10446b5352d390040fa7.bundle.js"
+  },
+  {
+    "url": "vendors~main.10446b5352d390040fa7.bundle.js"
+  },
+  {
+    "url": "vendors~main.10446b5352d390040fa7.bundle.js.LICENSE.txt"
+  }
+]);
